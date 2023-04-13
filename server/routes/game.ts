fd4b409bd91b24915524a0d95e569a3ae2fee29c
Sarah Lewis <sarahlewis1808@gmail.com>
2023-04-13 16:51:46 +1200

    create db, seed, migrate, testdata

--- server/routes/game.ts
+++ server/routes/game.ts
@@ -1,1 +1,12 @@
+import express from 'express'
+const router = express.Router()
 
+router.get('/', async (req, res) => {
+  //db function
+})
+
+router.post('/', async (req, res) => {
+  //db function
+})
+
+export default router
--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,12 @@
-// import express from 'express'
-// import { join } from 'node:path'
+import express from 'express'
+import { join } from 'node:path'
+import gameRoutes from './routes/game'
 
-// import fruitRoutes from './routes/game'
+const server = express()
 
-// const server = express()
+server.use(express.json())
+server.use(express.static(join(__dirname, 'public')))
 
-// server.use(express.json())
-// server.use(express.static(join(__dirname, 'public')))
+server.use('/api/v1/game', gameRoutes)
 
-// server.use('/api/v1/game', fruitRoutes)
-
-// export default server
+export default server
