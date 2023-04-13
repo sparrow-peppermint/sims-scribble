a11391e56fed278a532f51f5c519a6116224daf5
Ian Chua <ianchuajl@hotmail.com>
2023-04-13 17:10:07 +1200

    did all the app routes on front end with Sparrow

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,9 +1,25 @@
+import { Routes, Route } from 'react-router-dom'
+import Display from './Display'
+import Draw from './Draw'
+import Header from './Header'
+import Home from './Home'
+import Start from './Start'
+import Pass from './Pass'
+import Write from './Write'
+
 function App() {
   return (
     <>
-      <div className="bg-red-500">
-        <h1>Game</h1>
-      </div>
+        <h1>Its HAPPENING!!!!!!!!!!!</h1>
+      <Routes>
+        <Header />
+        <Home />
+        <Start />
+        <Pass />
+        <Draw />
+        <Write />
+        <Display />
+      </Routes>
     </>
   )
 }
---
+++ client/components/Start.tsx
@@ -0,0 +1,5 @@
+function Start() {
+  return <></>
+}
+
+export default Start
