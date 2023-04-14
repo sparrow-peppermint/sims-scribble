33d2f3153b8190210ae42cd44341809507e6d132
Milo Palmer <milogpalmer@gmail.com>
2023-04-14 15:29:23 +1200

    added base64 translation on canvas el

--- client/components/Canvas.test.tsx
+++ client/components/Canvas.test.tsx
@@ -1,8 +1,9 @@
 import { screen, render } from '@testing-library/react'
 import Canvas from './Canvas'
+import '@testing-library/jest-dom'
 
 test('Canvas renders a canvas element', () => {
   render(<Canvas width={700} height={500} />)
-  const canvas = screen.getByRole('canvas')
-  expect(canvas).toBeInTheDocumnet()
+  const canvas = screen.getByTestId('canvas')
+  expect(canvas).toBeInTheDocument()
 })
