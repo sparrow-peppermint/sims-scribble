8b7959d8f31868151842e5420a4125d8ddc35397
Sparrow Peppermint <sparrowpeppermint@gmail.com>
2023-04-16 15:29:52 +1200

    Add header styling

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,9 +1,24 @@
 function Header() {
   return (
     <>
-      <h1 className="font-header text-white text-6xl text-center p-4 mb-10">
-        SIMS SCRIBBLE
-      </h1>
+      <div className="container w-1/2 p-5">
+        {/* <h1 className="font-windows tracking-tighter text-4xl m-1/4">Sims</h1> */}
+        <div className="container flex flex-row items-center justify-center p-4">
+          <img
+            src="/images/windows-logo.png"
+            alt=""
+            className="w-20 pb-4 pr-2"
+          />
+
+          <p className="font-windows tracking-tighter text-3xl pr-3 pt-1.5 text-end align-bottom">
+            Sims
+          </p>
+          <h1 className="font-windows font-extrabold tracking-tighter text-5xl">
+            Scribble{' '}
+          </h1>
+          <p className="font-windows tracking-tighter text-6xl">98</p>
+        </div>
+      </div>
     </>
   )
 }
