0fe0712d25b8ad7e96681cfb4db891ccc4225a7f
Sparrow Peppermint <sparrowpeppermint@gmail.com>
2023-04-16 16:56:10 +1200

    Add home page styling

--- client/components/SubmitButton.tsx
+++ client/components/SubmitButton.tsx
@@ -14,7 +14,7 @@ function SubmitButton(props: PropsData) {
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <button type="submit" aria-label="submit">
+        <button type="submit" aria-label="submit" className="text-base">
           Submit
         </button>
       </form>
