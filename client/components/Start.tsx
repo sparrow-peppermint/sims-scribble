b1f5dd0e5c6660933ecc771480e939a63bce219d
Sparrow Peppermint <sparrowpeppermint@gmail.com>
2023-04-17 10:51:43 +1200

    Formatting layout

--- client/components/Start.tsx
+++ client/components/Start.tsx
@@ -12,26 +12,49 @@ function Start() {
   return (
     <>
       <div>
-        <p>
-          Heres where you enter a description as to what you want P2 to draw
-        </p>
-      </div>
-      <div>
-        <form>
-          <label>
-            What to draw:
-            <input
-              type="text"
-              onChange={handleChange}
-              placeholder="Enter prompt for user to draw"
-              value={input}
-            />
-          </label>
-        </form>
-        <SubmitButton
-          data={{ name: null, file: null, caption: input }}
-          id={1}
-        />
+        <div className="h-screen">
+          <div className="container flex justify-center pt-10">
+            <div className="window w-2/3">
+              <div className="title-bar">
+                <div className="title-bar-text text-base">
+                  Enter prompt for Player 2
+                </div>
+                <div className="title-bar-controls">
+                  <button aria-label="Minimize"></button>
+                  <button aria-label="Maximize"></button>
+                  <button aria-label="Close"></button>
+                </div>
+              </div>
+              <div className="window-body font-semibold text-base">
+                <p>
+                  Heres where you enter a description as to what you want P2 to
+                  draw
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="container flex justify-center field-row">
+            <form>
+              <label>
+                <h4 className="font-['Arial'] p-4 font-semibold">
+                  What to draw:
+                </h4>
+                <input
+                  type="text"
+                  onChange={handleChange}
+                  placeholder="Enter prompt for user to draw"
+                  value={input}
+                />
+              </label>
+            </form>
+            <div className="flex items-center h-auto ps-10 ">
+              <SubmitButton
+                data={{ name: null, file: null, caption: input }}
+                id={1}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
--- client/components/SubmitButton.tsx
+++ client/components/SubmitButton.tsx
@@ -14,7 +14,7 @@ function SubmitButton(props: PropsData) {
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <button type="submit" aria-label="submit" className="text-base">
+        <button type="submit" aria-label="submit" className="generalButton">
           Submit
         </button>
       </form>
