1aa634fadeadb1ff23fb4d8df7a2c722c4c48884
Sarah Lewis <sarahlewis1808@gmail.com>
2023-04-17 12:29:29 +1200

    add styling to final display page

--- client/components/Display.tsx
+++ client/components/Display.tsx
@@ -11,12 +11,38 @@ function Display() {
   }, [])
 
   return (
-    <div>
+    <div className="mb-10">
       {data.map(({ id, name, file, caption }) => {
         if (id % 2 == 0) {
-          return <img key={id} src={file} alt="" />
+          return (
+            <>
+              <div className="window flex justify-center flex-col">
+                <div className="window-body">
+                  <img key={id} src={file} alt="players drawing" />
+                </div>
+                <div className="m-8">
+                  <ul className="tree-view">caption</ul>
+                </div>
+              </div>
+            </>
+          )
         } else {
-          return <p key={id}>{caption}</p>
+          return (
+            <>
+              <div className="window">
+                <div className="title-bar">
+                  <p className="title-bar-text" key={id}>
+                    {caption}
+                  </p>
+                  <div className="title-bar-controls">
+                    <button aria-label="Minimize"></button>
+                    <button aria-label="Maximize"></button>
+                    <button aria-label="Close"></button>
+                  </div>
+                </div>
+              </div>
+            </>
+          )
         }
       })}
     </div>
