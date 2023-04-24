c8c6778132b0f8d74a6136f4b4d0044d585a5283
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-04-25 07:32:15 +1200

    connect client to server

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import { socket } from '../socket'
 import Display from './Display'
 import Draw from './Draw'
 import Header from './Header'
@@ -6,15 +7,53 @@ import Home from './Home'
 import Start from './Start'
 import Pass from './Pass'
 import Write from './Write'
+import { useEffect, useState } from 'react'
+
+interface Message {
+  text: string
+}
 
 function App() {
+  const [isConnected, setIsConnected] = useState(socket.connected)
+  const [messages, setMessages] = useState([] as Message[])
+
+
+  useEffect(() => {
+
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    function onFooEvent(value: Message) {
+      setMessages(previous => [...previous, value]);
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+    socket.on('foo', onFooEvent);
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+      socket.off('foo', onFooEvent);
+    };
+  }, []);
+
   return (
     <div className="min-h-screen bg-cover bg-teal">
+      <p>Is Connected: {isConnected ? 'connected' : 'disconnected'}</p>
+      <button onClick={() => {
+        console.log('clicked');
+        socket.connect()
+      }}>Connect</button>
       <Header />
 
       <div className="flex flex-col mx-48">
         <Routes>
-          <Route path="/" element={<Home />} />
           <Route path="/start" element={<Start />} />
           <Route path="/pass/:id" element={<Pass />} />
           <Route path="/draw/:id" element={<Draw />} />
