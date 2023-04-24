53a1d90dce843e28b70b77030ce62bdbd08ac936
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-04-24 14:15:43 +1200

    configure socket on server

--- server/server.ts
+++ server/server.ts
@@ -1,17 +1,24 @@
 import express from 'express'
 import { join } from 'node:path'
-
+import http from 'http'
+import { Server } from 'socket.io'
 import gameRoutes from './routes/game'
 
 const server = express()
+const httpServer = http.createServer(server)
+const io = new Server(httpServer)
 
 server.use(express.json())
 server.use(express.static(join(__dirname, 'public')))
 
 server.use('/api/v1/game', gameRoutes)
 
-server.get('*', (req, res) => {
+server.get('*', (_, res) => {
   res.sendFile(join(__dirname, 'public/index.html'))
+})
+
+io.on('connection', (socket) => {
+  console.log(`a user connected on socket: ${socket}`)
 })
 
 export default server
